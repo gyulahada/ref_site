5a4c7e21c2a7675f38925c115dc084fe412bc0a9
Gyuszi <hadagyula@gmail.com>
2023-09-18 17:24:23 +0200

    hashrouter test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import HeroesPage from './pages/HeroesPage';
 
 export default function App() {
   return (
-    <Router basename="/ref_site">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/HeroesPage" element={<HeroesPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
