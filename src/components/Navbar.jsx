4777ed44de1ae727c029073a784108baf11955e7
Gyuszi <hadagyula@gmail.com>
2023-09-19 16:57:40 +0200

    change a tag to link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="container mx-auto px-6 py-3 mt-2 md:flex md:justify-between md:items-center relative">
       <div className="flex justify-between items-center">
         <div>
-          <a href="/" className="text-xl font-bold text-white">BJJ</a>
+          <Link to="/" className="text-xl font-bold text-white">BJJ</Link>
         </div>
         <div className="flex md:hidden">
-          <button onClick={() => setIsOpen(!isOpen)} type="button" className="text-white hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
+          <button onClick={toggleMenu} type="button" className="text-white hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
             <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
               <path fillRule="evenodd" d="M4 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1zm0 7a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1zm1 6a1 1 0 1 0 0 2h14a1 1 0 1 0 0-2H5z"></path>
             </svg>
@@ -19,11 +24,9 @@ const Navbar = () => {
       </div>
       <div className={`${isOpen ? 'block' : 'hidden'} md:flex items-center justify-end absolute w-full bg-black md:relative md:bg-transparent`}>
         <div className="flex flex-col md:flex-row md:mx-6">
-          <a href="/" className="my-1 text-white pt-1.5 md:mx-4 md:my-0">Home</a>
-          <a href="#" className="my-1 text-white pt-1.5 md:mx-4 md:my-0">Contact</a>
-          <div className='w-100 md:flex md:justify-between md:items-center mt-4 md:mt-0 md:ml-0 -ml-2.5'>
-            <a href="./HeroesPage" className="my-1 text-black bg-white rounded p-1.5 px-2.5 hover:bg-black hover:text-white mt-4 md:mt-0 md:mx-4 md:my-0">Heroes</a>
-          </div>
+          <Link to="/" className="my-1 text-white pt-1.5 md:mx-4 md:my-0">Home</Link>
+          <Link to="/contact" className="my-1 text-white pt-1.5 md:mx-4 md:my-0">Contact</Link>
+          <Link to="/heroes" className="my-1 text-black bg-white rounded p-1.5 px-2.5 hover:bg-black hover:text-white mt-4 md:mt-0 md:mx-4 md:my-0">Heroes</Link>
         </div>
       </div>
     </div>
